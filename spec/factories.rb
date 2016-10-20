31c92c99f6177ad70261cc966736b1fc16872538
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-10-20 15:32:46 -0600

    setup categories index page

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,4 @@
 FactoryGirl.define do
-  factory :category do
-    title "MyString"
-  end
 
   factory :company do
     name "Some Company"
@@ -23,6 +20,16 @@ FactoryGirl.define do
     sequence :level_of_interest do |n|
       n
     end
+  end
 
+  factory :category do
+
+    sequence :title do |n|
+      "Sweet Jobs #{n}"
+    end
+
+    factory :category_with_jobs do
+      jobs {create_list(:job, 3)}
+    end
   end
 end
