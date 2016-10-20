07d86cdf22666fd22d5400a1d2ebf0bb795d386d
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-10-20 15:01:30 -0600

    created and tested category jobs relationship

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,17 +2,28 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   describe "validations" do
+    context "valid attributes" do
 
-    it "is valid with title" do
+      it "is valid with title" do
+        category = Category.new(title: "cool jobs")
+
+        expect(category).to be_valid
+      end
+    end
+    context "invalid attributes" do
+      it "is invalid without title" do
+        category = Category.new()
+
+        expect(category).to be_invalid
+      end
+    end
+  end
+
+  describe "relationships" do
+    it "has_many jobs" do
       category = Category.new(title: "cool jobs")
 
-      expect(category).to be_valid
-    end
-
-    it "is invalid without title" do
-      category = Category.new()
-
-      expect(category).to be_invalid
+      expect(category).to respond_to(:jobs)
     end
   end
 end
