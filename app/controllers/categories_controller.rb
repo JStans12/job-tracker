e092d8fbec8c554022e75b8b5fbca7c23ac663cb
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-10-21 10:16:59 -0600

    user can delete a category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -32,6 +32,9 @@ class CategoriesController < ApplicationController
   end
 
   def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_path
   end
 
   private
